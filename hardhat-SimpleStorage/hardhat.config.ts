b127c18149cd3bbe3f493947460d77982fc94a38
BurhanRaja <burhanraja02@yahoo.com>
2023-08-19 10:38:04 +0530

    deployed contract with hardhat on sepolia testnet

--- hardhat-SimpleStorage/hardhat.config.ts
+++ hardhat-SimpleStorage/hardhat.config.ts
@@ -1,7 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const sepoliaURL = process.env.SEPOLIA_RPC_URL!;
+const sepoliaPrivateKey = process.env.SEPOLIA_PRIVATE_KEY!;
 
 const config: HardhatUserConfig = {
+  networks: {
+    sepolia: {
+      url: sepoliaURL,
+      accounts: [sepoliaPrivateKey],
+      chainId: 11155111,
+    },
+  },
   solidity: "0.8.19",
 };
 
--- hardhat-SimpleStorage/scripts/deploy.ts
+++ hardhat-SimpleStorage/scripts/deploy.ts
@@ -1,27 +1,20 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+  console.log("Deploying Contract. Please Wait....");
 
-  const lockedAmount = ethers.parseEther("0.001");
+  const simpleStorage = await SimpleStorageFactory.deploy();
+  // const deployedCode = await simpleStorage.getDeployedCode();
+  const address = await simpleStorage.getAddress();
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(`Contract Address: ${address}`);
+  // console.log(`Contract Deployed Code: ${deployedCode}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
