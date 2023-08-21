5640df04e04d23fc708d541785570f1f4f4f1b36
BurhanRaja <burhanraja02@yahoo.com>
2023-08-21 08:51:57 +0530

    added gas reporter and coinmarketcap API

--- hardhat-SimpleStorage/hardhat.config.ts
+++ hardhat-SimpleStorage/hardhat.config.ts
@@ -2,11 +2,13 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-verify";
 import "./tasks/block-number";
+import "hardhat-gas-reporter";
 import "dotenv/config";
 
 const sepoliaURL = process.env.SEPOLIA_RPC_URL!;
 const sepoliaPrivateKey = process.env.SEPOLIA_PRIVATE_KEY!;
 const etherscanAPIKey = process.env.ETHERSACN_API_KEY!;
+const coinmarketCapAPIKey = process.env.COINMARKETCAP_API_KEY!;
 
 const config: HardhatUserConfig = {
   networks: {
@@ -24,6 +26,14 @@ const config: HardhatUserConfig = {
   solidity: "0.8.19",
   etherscan: {
     apiKey: etherscanAPIKey,
+  },
+  // Gives an output in gas-repoter.txt file
+  gasReporter: {
+    enabled: false, // false when not working
+    outputFile: "./gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: coinmarketCapAPIKey,
   },
 };
 
