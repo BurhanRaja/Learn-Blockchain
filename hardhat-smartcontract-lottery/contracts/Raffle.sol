299495703f5f90651393fd5ab1866bed2f5a1562
BurhanuddinRaja <mails4burhan@gmail.com>
2023-09-25 18:32:48 +0530

    added raffle contract start

--- hardhat-smartcontract-lottery/contracts/Raffle.sol
+++ hardhat-smartcontract-lottery/contracts/Raffle.sol
@@ -2,8 +2,29 @@
 pragma solidity ^0.8.18;
 
 import "@chainlink/contracts/v0.8/vrf/VRFConsumerBaseV2.sol";
-import "@chainlink/contracts/v0.8/vrf/VRFCoordinatorV2.sol";
+import "@chainlink/contracts/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
-contract Raffle {
-    
+error Raffle_SendMoreToEnterRaffle();
+
+contract Raffle is VRFConsumerBaseV2 {
+    uint256 private immutable i_entranceFeed;
+    address payable[] private s_players;
+
+    events RaffleEnter(address indexed player);
+
+    constructor(uint256 entranceFeed) {
+        i_entranceFeed = entranceFeed;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFeed) {
+            revert Raffle_SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender));
+        RaffleEnter(msg.sender);
+    };
+
+    function fullfillRandomWords() internal override {
+        
+    };
 }
