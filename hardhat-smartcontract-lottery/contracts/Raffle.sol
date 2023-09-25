75c4ffa559c007182810f5070475e363827e30b8
BurhanRaja <burhanraja02@yahoo.com>
2023-09-25 22:12:54 +0530

    added Raffle contract

--- hardhat-smartcontract-lottery/contracts/Raffle.sol
+++ hardhat-smartcontract-lottery/contracts/Raffle.sol
@@ -1,30 +1,91 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.18;
 
-import "@chainlink/contracts/v0.8/vrf/VRFConsumerBaseV2.sol";
-import "@chainlink/contracts/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 
-error Raffle_SendMoreToEnterRaffle();
+error Raffle__NotEnoughEthEntered();
+error Raffle__TransferFailed();
 
 contract Raffle is VRFConsumerBaseV2 {
-    uint256 private immutable i_entranceFeed;
+    // VRF Variables
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private immutable NUM_WORDS = 1;
+
+    // Storage
+    uint256 private immutable i_entraceFeed;
     address payable[] private s_players;
+    address private s_recentWinner;
 
-    events RaffleEnter(address indexed player);
+    // Events
+    event RequestedRaffleWinner(uint256 indexed requestId);
+    event RaffleEnter(address indexed player);
+    event WinnerSelected(address indexed player);
 
-    constructor(uint256 entranceFeed) {
-        i_entranceFeed = entranceFeed;
+    constructor(
+        uint256 entraceFeed,
+        address vrfCoordinatorV2,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_entraceFeed = entraceFeed;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
-        if (msg.value < i_entranceFeed) {
-            revert Raffle_SendMoreToEnterRaffle();
+        if (msg.value < i_entraceFeed) {
+            revert Raffle__NotEnoughEthEntered();
         }
         s_players.push(payable(msg.sender));
-        RaffleEnter(msg.sender);
-    };
+        emit RaffleEnter(msg.sender);
+    }
 
-    function fullfillRandomWords() internal override {
-        
-    };
+    // Request Random Winner
+    function requestRandomWinner() external {
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestedRaffleWinner(requestId);
+    }
+
+    // Get a Recent Winner
+    function fulfillRandomWords(
+        uint256,
+        uint256[] memory randomWords
+    ) internal virtual override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+        emit WinnerSelected(recentWinner);
+    }
+
+    // Get Functions
+    function getEntranceFeed() public view returns (uint256) {
+        return i_entraceFeed;
+    }
+
+    function getOnePlayer(uint256 index) public view returns (address payable) {
+        return s_players[index];
+    }
+
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
+    }
 }
