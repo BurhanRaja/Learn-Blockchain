16531eae5addc6de14b23d6c094dbcbf8262a0ca
BurhanRaja <burhanraja02@yahoo.com>
2023-09-26 21:39:20 +0530

    completed contract and started setup for deploy

--- hardhat-smartcontract-lottery/contracts/Raffle.sol
+++ hardhat-smartcontract-lottery/contracts/Raffle.sol
@@ -4,11 +4,29 @@ pragma solidity ^0.8.18;
 
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/automation/interfaces/KeeperCompatibleInterface.sol";
 
 error Raffle__NotEnoughEthEntered();
 error Raffle__TransferFailed();
+error Raffle__NotOpen();
+error Raffle__UpKeepNotNeeded(
+    uint256 balance,
+    uint256 players,
+    uint256 raffleState
+);
 
-contract Raffle is VRFConsumerBaseV2 {
+/**
+ * @title Sample Raffle Contract
+ * @author Burhanuddin Raja
+ */
+
+contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
+    // Enum State
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+
     // VRF Variables
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
     bytes32 private immutable i_gasLane;
@@ -21,6 +39,9 @@ contract Raffle is VRFConsumerBaseV2 {
     uint256 private immutable i_entraceFeed;
     address payable[] private s_players;
     address private s_recentWinner;
+    RaffleState private s_raffleState;
+    uint256 private s_lastTimeStamp;
+    uint256 private immutable i_interval;
 
     // Events
     event RequestedRaffleWinner(uint256 indexed requestId);
@@ -32,25 +53,64 @@ contract Raffle is VRFConsumerBaseV2 {
         address vrfCoordinatorV2,
         bytes32 gasLane,
         uint64 subscriptionId,
-        uint32 callbackGasLimit
+        uint32 callbackGasLimit,
+        uint256 interval
     ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entraceFeed = entraceFeed;
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_gasLane = gasLane;
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
+        s_raffleState = RaffleState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        i_interval = interval;
     }
 
+    // Add Money
     function enterRaffle() public payable {
         if (msg.value < i_entraceFeed) {
             revert Raffle__NotEnoughEthEntered();
+        }
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__NotOpen();
         }
         s_players.push(payable(msg.sender));
         emit RaffleEnter(msg.sender);
     }
 
+    // CheckUpKeep Function
+    /**
+     * @dev to check whether the Lottery timings are over or not
+     */
+
+    function checkUpkeep(
+        bytes memory /*checkData*/
+    )
+        public
+        view
+        override
+        returns (bool upkeepNeeded, bytes memory /* performData */)
+    {
+        bool isOpen = (RaffleState.OPEN == s_raffleState);
+        // block.timestamp = last block timestamp > interval
+        bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
+        bool hasPlayers = (s_players.length > 0);
+        bool hasBalance = address(this).balance > 0;
+        upkeepNeeded = (isOpen && timePassed && hasPlayers && hasBalance);
+        return (upkeepNeeded, "");
+    }
+
     // Request Random Winner
-    function requestRandomWinner() external {
+    function performUpkeep(bytes calldata /* performData */) external override {
+        (bool upKeepNeeded, ) = checkUpkeep("");
+        if (!upKeepNeeded) {
+            revert Raffle__UpKeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint256(s_raffleState)
+            );
+        }
+        s_raffleState = RaffleState.CALCULATING;
         uint256 requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLane,
             i_subscriptionId,
@@ -65,10 +125,13 @@ contract Raffle is VRFConsumerBaseV2 {
     function fulfillRandomWords(
         uint256,
         uint256[] memory randomWords
-    ) internal virtual override {
+    ) internal override {
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
         s_recentWinner = recentWinner;
+        s_raffleState = RaffleState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
         (bool success, ) = recentWinner.call{value: address(this).balance}("");
         if (!success) {
             revert Raffle__TransferFailed();
@@ -88,4 +151,25 @@ contract Raffle is VRFConsumerBaseV2 {
     function getRecentWinner() public view returns (address) {
         return s_recentWinner;
     }
+
+    function getRaffleState() public view returns (RaffleState) {
+        return s_raffleState;
+    }
+    // pure - not in storage but in a contract;
+    function getNumberOfWords() public pure returns(uint256) {
+        return NUM_WORDS;
+    }
+
+    function getTotalPlayers() public view returns(uint256) {
+        return s_players.length;
+    }
+
+    function getLastTimeStamp() public view returns(uint256) {
+        return s_lastTimeStamp;
+    }
+
+    function getRequestConfirmations() public pure returns(uint256) {
+        return REQUEST_CONFIRMATIONS;
+    }
+
 }
--- hardhat-smartcontract-lottery/hardhat.config.ts
+++ hardhat-smartcontract-lottery/hardhat.config.ts
@@ -6,9 +6,37 @@ import "hardhat-gas-reporter";
 import "hardhat-contract-sizer";
 import "dotenv/config";
 
+const sepoliaURL = process.env.SEPOLIA_RPC_URL!;
+const sepoliaPrivateKey = process.env.SEPOLIA_PRIVATE_KEY!;
+const etherscanAPIKey = process.env.ETHERSCAN_API_KEY!;
+const coinmarketCapAPIKey = process.env.COINMARKETCAP_API_KEY!;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    sepolia: {
+      url: sepoliaURL,
+      accounts: [sepoliaPrivateKey],
+      chainId: 11155111,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
 };
 
 export default config;
